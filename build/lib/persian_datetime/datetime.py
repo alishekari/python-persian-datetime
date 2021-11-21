f7307b42e7fbe9c447e04ee3e8bb5f957fb79467
Ali Shekari <alishekari1991@outlook.com>
2021-11-21 18:40:36 +0330

    ver 10.1

--- build/lib/persian_datetime/datetime.py
+++ build/lib/persian_datetime/datetime.py
@@ -17,6 +17,21 @@ class Jalali:
         Thursday = 'پنجشنبه',
         Friday = 'جمعه',
 
+    jalali_months = {
+        1: 'فروردین',
+        2: 'اردیبهشت',
+        3: 'خرداد',
+        4: 'تیر',
+        5: 'مرداد',
+        6: 'شهریور',
+        7: 'مهر',
+        8: 'آبان',
+        9: 'آذر',
+        10: 'دی',
+        11: 'بهمن',
+        12: 'اسفند',
+    }
+
     class JalaliSerializer(serializers.BaseSerializer):
         def to_representation(self, instance):
             return {
